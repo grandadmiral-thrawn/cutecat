3eedac8a0e461b81933d26d646b71affa881a869
James Masologites <masologites@gmail.com>
2015-03-08 18:06:50 -0400

    database stuff

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
 
-app.get('/', function(request, response) {
-  response.send(cool());
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
+
+app.get('/', function (request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+        result += cool();
+  response.send(result);
 });
 
 app.listen(app.get('port'), function() {
