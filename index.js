f60b53d63d1b5232cefd2a5679d6bcdcba1fcf15
Jack Peterson <jack@tinybike.net>
2015-03-08 22:31:21 -0700

    postgres stuff

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3700;
 var io = require('socket.io').listen(app.listen(port));
+var pg = require('pg');
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/templates');
@@ -16,6 +17,16 @@ app.set('view engine', 'ejs');
 app.get("/", function (req, res) {
     res.render("page");
 });
+
+app.get('/db', function (req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('SELECT * FROM test_table', function (err, result) {
+            done();
+            if (err) console.error(err);
+            res.send(result.rows);
+        });
+    });
+})
 
 // Websocket listeners
 
